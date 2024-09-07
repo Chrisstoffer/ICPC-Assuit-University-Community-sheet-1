ba571eefd72e19b1625f4e10711237bbba635d3c
Anik Das <141364233+Chrisstoffer@users.noreply.github.com>
2024-09-07 15:53:21 +0600

    Add files via upload

---
+++ BasicDataType.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    long long lli;
+    char c;
+    float f;
+    double d;
+
+    cin>>i>>lli>>c>>f>>d;       //Taking all input in one line
+    cout<<i<<endl;
+    cout<<lli<<endl;
+    cout<<c<<endl;              //Printing input one after another
+    cout<<f<<endl;
+    cout<<d<<endl;
+
+
+
+
+}
